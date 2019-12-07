2d8a40ce1f46ff4b0b35ed57296e68719645ea73
Sergio Torres <sergio.uft@gmail.com>
2019-12-07 17:03:36 -0300

    chore(fix): fix stickler errors

--- src/components/App.js
+++ src/components/App.js
@@ -14,12 +14,12 @@ class App extends Component {
     };
   }
 
-  handleClickBtn = (button) => {
+  handleClickBtn(button) {
     const data = calculate(this.state, button);
     this.setState(data);
   }
 
-  resultToStr() {
+  resultToStr = () => {
     const { total, next, operation } = this.state;
     let result = `${total}${operation}${next}`.replace(/null/g, '');
     return result === '' ? undefined : result;
