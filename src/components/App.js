52f95dce1610cd561e6464b27982023dfe14a816
Sergio Torres <sergio.uft@gmail.com>
2019-12-07 13:24:39 -0300

    feature(app): integrate app component with calculator logics

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import calculate from '../logic/calculate';
 import '../App.css';
 
 class App extends Component {
@@ -14,13 +15,20 @@ class App extends Component {
   }
 
   handleClickBtn = (button) => {
-    console.log(button);
+    const data = calculate({ ...this.state }, button);
+    this.setState({ ...data });
+  }
+
+  resultToStr = () => {
+    const { total, next, operation } = this.state;
+    let result = `${total}${operation}${next}`.replace(/null/g, '');
+    return result === '' ? undefined : result;
   }
 
   render() {
     return (
       <div className="App flex">
-        <Display />
+        <Display result={this.resultToStr()} />
         <ButtonPanel handleClick={this.handleClickBtn} />
       </div>
     );
